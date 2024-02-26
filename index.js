c3e22946052ffbbdc4b8eb507269cb65b56eb693
App God <phillymantis@gmail.com>
2024-02-25 22:22:42 -0500

    support capitalized emails

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ myBot.client.once(Events.ClientReady, readyClient => {
     deleteBotMessagesToUser('408163545830785024');
 });
 
+
+
 /*
 myBot.client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
@@ -136,7 +138,7 @@ myBot.client.on('messageCreate', async (message) => {
           ]
 
 		const createChatObject = {
-			model: 'gpt-3.5-turbo',
+			model: 'gpt-4',
 			messages,
             functions,
             function_call: 'auto',
@@ -166,7 +168,7 @@ myBot.client.on('messageCreate', async (message) => {
       
       
             const secondResponse = await openai.chat.completions.create({
-              model: 'gpt-3.5-turbo',
+              model: 'gpt-4',
               messages,
             functions,
             function_call: 'auto',
